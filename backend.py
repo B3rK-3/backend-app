35452b536b668d6c47831e7ed242c08160ed0a7e
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2025-09-19 13:09:37 -0400

    host

--- backend.py
+++ backend.py
@@ -349,4 +349,4 @@ JOIN filtered f ON f.id = g.id
 ORDER BY dist
 LIMIT 20;"""
 if __name__ == "__main__":
-    app.run(debug=True, host="192.168.1.217", port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
